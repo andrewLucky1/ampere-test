9c53bb51220af87311504c71d55e30d372e91ecd
z0uR <andrey.furman.sup.acc@gmail.com>
2018-06-08 10:03:49 +0300

    parser added && some sync improvements added to app.js

--- app.js
+++ app.js
@@ -5,22 +5,32 @@ const AppServices = require('./acme/AppServices');
 
 // comment about this yargs
 argv.command(`parse-ampere`,
-    `Retrieves all the posts from the page and ✏️ to the log file`, () => { }, argv => {
-        var url = `https://www.ampereanalysis.com/`;
-        // comment about jsdom
+    `Retrieves all the posts from the page and ✏️ to the log file`, {
+        url: {
+            alias: 'u',
+            default: `https://www.ampereanalysis.com/`
+        }
+    }, argv => {
+        console.log(`\x1b[36m ${argv.url} \x1b[0m`);
+        // Using jsdom package to parse received dom
         JSDOM.fromURL(url, {
             runScripts: 'dangerously'
         }).then(dom => {
             var appServices, initialServicesParams;
             initialServicesParams = { path: path.join(__dirname, 'logs/app.json'), dom };
-            appServices = new AppServices(initialServicesParams);
-            // 📥 Gather all the necessary information about the posts
-            var postsInfo = appServices.getPostsInfo();
-            // 📝  Write posts data into a log file
-            appServices.writeToLogFile(postsInfo);
+            //  ↩️ guarantee sequence execution of posts information receiving and writing it into log file
+            sequenceExecution(initialServicesParams);
         }, err => {
             console.log(`\x1b[35m ${err} \x1b[0m`);
         });
     })
     .help()
     .argv;
+
+async function sequenceExecution(initialServicesParams) {
+    appServices = new AppServices(initialServicesParams);
+    // 📥 Gather all the necessary information about the posts
+    var postsInfo = await appServices.getPostsInfo();
+    // 📝  Write posts data into a log file
+    await appServices.writeToLogFile(postsInfo);
+}
